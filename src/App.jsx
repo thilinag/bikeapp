020275fac195dc318b434c7e1ca4f6299517092d
Thilina <thilina@aligent.com.au>
2021-02-14 20:12:55 +1030

    add speedometer to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css';
+import Speedometer from './components/Speedometer/Speedometer';
 
 function App() {
   return (
     <div className="App">
+      <div className="SpeedometerWrapper">
+        <Speedometer />
+      </div>
     </div>
   );
 }

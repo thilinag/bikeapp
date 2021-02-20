3530f4efaf594257e517595a1f73079b803b7fdf
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2021-02-20 19:12:21 +1030

    set speed to state as a decimal with 2 places

--- src/components/Speedometer/hooks/useSpeedometer.js
+++ src/components/Speedometer/hooks/useSpeedometer.js
@@ -3,14 +3,14 @@ import { msToKmh } from 'utils/msToKmh';
 
 export const useSpeedometer = () => {
 
-    const [speed, setSpeed] = useState('0.0');
+    const [speed, setSpeed] = useState(0);
     const [hasErrors, setHasErrors] = useState(false);
     const [maxSpeed, setMaxSpeed] = useState(0);
 
     useEffect(() => {
         navigator.geolocation.watchPosition(
             position => {
-                const currentSpeed = position.coords.speed;
+                const currentSpeed = Math.round(position.coords.speed * 100) / 100;
                 if (currentSpeed && currentSpeed > 0.25) {
                     // speed is in meters per second
                     setSpeed(currentSpeed);
